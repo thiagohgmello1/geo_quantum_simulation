5cc1644bc08b62f4477065cb1678288e4eb44aca
Thiago Mello <thiagohgmello@gmail.com>
2023-04-13 08:38:14 -0300

    Separate constants to specific file

---
+++ setup_func/constants.m
@@ -0,0 +1,14 @@
+%% Constants
+
+a = 1.42e-10;
+e_0 = 1 / (36 * pi * 1e9);
+q = -1.60217663 * 1e-19;
+scale = 1e-9;
+eq_fermi_energy = 0.1;
+kB = 8.6173324 * 1e-5; % Boltzmann constant in eV/K
+energy_points = 100;
+
+% Material properties
+graphene_angle = 90;
+n_sides = 6;
+geometry_angle = 0;
